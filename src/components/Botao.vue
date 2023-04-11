27225affc0dc4e7ef23b7dbe05ad71ffda12c7e0
Guilherme Filipe Da Rosa <guilhermefdarosa15@gmail.com>
2023-04-10 23:26:03 -0300

    fim curso

---
+++ src/components/Botao.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="button" @click="$emit('clicado')" :disabled="disabled">
+            <span class="icon">
+                <i class="fas" :class="icon"></i>
+            </span>
+            <span>{{ nome }}</span>
+        </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "BotaoTracker",
+    emits: ["clicado"],
+    props: {
+        disabled: Boolean,
+        nome: String,
+        icon: String,
+    }
+})
+</script>
