779afb01e9642e0d9ca2129ab4208deb47e01d3c
Guilherme Filipe Da Rosa <guilhermefdarosa15@gmail.com>
2023-08-30 19:12:58 -0300

    avançando no framework

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -3,7 +3,25 @@
     <h1>
       <img src="../assets/logo.png" alt="">
     </h1>
-    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    <div class="has-text-centered">
+      <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -20,7 +38,7 @@ export default defineComponent({
   },
   computed: {
     textoBotao() {
-      return this.modoEscuroAtivo ? 'Desativar modo escuro': 'Ativar modo escuro';
+      return this.modoEscuroAtivo ? 'Desativar modo escuro' : 'Ativar modo escuro';
     },
   },
   methods: {
@@ -35,14 +53,31 @@ export default defineComponent({
 header {
   background: #0d3b66;
   width: 100%;
-  height: 100vh; 
+  height: 100vh;
   padding: 1rem;
   text-align: center;
 }
+
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #FAF0CA;
+}
+
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="box formulario">
         <div class="coluns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?"
                 v-model="descricao">
+            </div>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProjeto">
+                    <option value="">Selecione o projeto</option>
+                    <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">{{ projeto.nome }}</option>
+                    </select>
+                </div>
             </div>
             <div class="column">
                 <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
@@ -12,8 +20,10 @@
     </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue';
 import Temporizador from './Temporizador.vue';
+import { useStore } from 'vuex';
+import { key } from '@/store';
 
 export default defineComponent({
     name: 'FormularioTracker',
@@ -24,17 +34,25 @@ export default defineComponent({
     data() {
         return {
             descricao: '',
+            idProjeto: '',
         };
     },
     methods: {
         finalizarTarefa (tempoEmSegundos: number) : void {
             this.$emit('aoSalvarTarefa', {
                 duracaoEmSegundos: tempoEmSegundos,
-                descricao: this.descricao
+                descricao: this.descricao,
+                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
             });
 
             this.descricao = '';
         },
+    },
+    setup () {
+        const store = useStore(key)
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
     },
 })
 </script>
